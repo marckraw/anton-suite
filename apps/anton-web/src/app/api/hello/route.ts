ac1dfc58848b8379491732781fbc1f4afb157644
Marcin Krawczyk <marckraw@icloud.com>
2024-08-09 12:04:19 +0200

    fix(anton-web): properly return when env empty

--- apps/anton-web/src/app/api/hello/route.ts
+++ apps/anton-web/src/app/api/hello/route.ts
@@ -3,13 +3,10 @@ import {AntonSDK} from "@mrck-labs/anton-sdk-test";
 export async function GET(request: Request) {
     const apiKey = process.env.ANTHROPIC_API_KEY;
 
-    if(!apiKey) return {status: 401, body: 'API key not found'};
-
-    console.log('This is api key ? ');
-    console.log(apiKey);
-
-    console.info('logging from AppService.getCompletion()');
-
+    if(!apiKey) {
+        return Response.json({status: 401, body: 'API key not found'});
+    }
+    
     const anton = new AntonSDK({ type: 'anthropic', apiKey: apiKey });
 
     const response = await anton.chat([
