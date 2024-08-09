8a6e4aaa3e7ebc14400fd14d1bb66aaef40c37b4
Marcin Krawczyk <marckraw@icloud.com>
2024-08-09 11:41:22 +0200

    nextjs

---
+++ apps/anton-web/src/app/api/hello/route.ts
@@ -0,0 +1,21 @@
+import {AntonSDK} from "@mrck-labs/anton-sdk-test";
+
+export async function GET(request: Request) {
+    const apiKey = process.env.ANTHROPIC_API_KEY;
+
+    console.log('This is api key ? ');
+    console.log(apiKey);
+
+    console.info('logging from AppService.getCompletion()');
+
+    const anton = new AntonSDK({ type: 'anthropic', apiKey: apiKey });
+
+    const response = await anton.chat([
+        {
+            content: 'Hey there, how are you ? :)',
+            role: 'user',
+        },
+    ]);
+
+    return Response.json({data: response})
+}
--- apps/vite-react-app/src/App.tsx
+++ apps/vite-react-app/src/App.tsx
@@ -1,15 +1,11 @@
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {someFunc} from '@mrck-labs/utils'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-    useEffect(() => {
-        someFunc()
-    })
   return (
     <>
       <div>
@@ -30,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more asdas
       </p>
     </>
   )
